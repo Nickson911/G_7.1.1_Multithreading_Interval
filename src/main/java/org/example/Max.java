5de732629feca99a70816e8d3af76b6ce0714111
Neto test <denis.157911@mail.ru>
2024-05-07 13:41:26 +0400

    max interval

---
+++ src/main/java/org/example/Max.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Max {
+
+    public static void main() throws InterruptedException, ExecutionException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        List<Future<Integer>> futureList = new ArrayList<>();
+        int maxValue = 0;
+        ExecutorService threadPool = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors());
+
+        long startTs = System.currentTimeMillis(); // start time
+
+        for (String text : texts) {
+            futureList.add(threadPool.submit(new CallableTask(text)));
+        }
+
+        for (Future<Integer> future : futureList) {
+            maxValue = Math.max(future.get(), maxValue);
+        }
+        threadPool.shutdown();
+        System.out.println("Максимальное значение - " + maxValue);
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
+
+class CallableTask implements Callable<Integer> {
+    private final String text;
+
+    public CallableTask(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public Integer call() {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        return maxSize;
+    }
+}
